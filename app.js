ecb8cb3337c9feef29380d9462ecd7e9a486892a
Your Name <Sanwicklith>
2024-08-19 18:18:12 +0200

    Exercise 4.2 done

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const app = express()
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const cors = require('cors')
-const middleware = require('./utils/middleware')
+const express = require('express');
 
-const mongoose = require('mongoose')
+const app = express();
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const cors = require('cors');
+const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+logger.info('connect to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 // middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+app.use(middleware.errorHandler);
+app.use(middleware.requestLogger);
+app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(config.PORT, ()=>{
-  logger.info(`server is running on port ${config.PORT}`)
-})
-
-
-module.exports = app
+module.exports = app;
