b0dcf88ad56809b808d086f0cb43108dddec7e0d
Your Name <Sanwicklith>
2024-08-19 14:18:13 +0200

    add code to config.js and export PORT and MONGODB_URI

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const MONGODB_URI = 'mongodb+srv://mbafin23:EY2PiHLd3t3RpS9Z@blog.j4yqeex.mongodb.net/?retryWrites=true&w=majority&appName=blog'
+
+const PORT = 3003
+
+
+module.exports={
+  MONGODB_URI,
+  PORT
+}
