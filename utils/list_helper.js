3ac5caff44397d1eccebdb72e57933f8e318003d
Your Name <Sanwicklith>
2024-08-19 22:51:15 +0200

    Exercise 4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,21 @@
-const dummy  = (blog)=>{
-  if (blog){
-    return 1
+const dummy = blog => {
+  if (blog) {
+    return 1;
   }
-}
+};
 
-module.exports  ={
-  dummy
-}
+const totalLikes = blog => {
+  const likes = blog.map(blog => blog.likes);
+  if (likes.length === 0) {
+    return 0;
+  } else if (likes.length === 1) {
+    return likes[0];
+  } else {
+    return likes.reduce((a, b) => a + b, 0);
+  }
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
