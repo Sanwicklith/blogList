8b2ab0bf34f6c32dedc9057c219edabe68ebcbbf
Your Name <Sanwicklith>
2024-08-23 00:02:57 +0200

    "Exercise 4.12 done, Added express-async-errors package and implemented error handling for missing title or URL in blog creation"

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+require('express-async-errors')
 
 blogsRouter.get('/', async (request, response) => {
   const res = await Blog.find({});
@@ -8,8 +9,13 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'Title or URL is missing' });
+  }
+
   const blog = new Blog({
-    title: body.title,
+    title: body.title || 'Unknown',
     author: body.author,
     url: body.url,
     likes: body.likes || 0
